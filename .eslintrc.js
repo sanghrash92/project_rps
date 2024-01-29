f0dae62ccabe6da644ae79c21a47153b9c0d1962
Sanji <sanghrash92@gmail.com>
2024-01-29 19:50:15 +0000

    Added icon feature

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,12 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
-    },
-    "extends": "eslint:recommended",
-    "overrides": [
-    ],
-    "parserOptions": {
-        "ecmaVersion": "latest"
-    },
-    "rules": {
-    }
-}
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: "eslint:recommended",
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: "latest",
+  },
+  rules: {},
+};
--- javascript.js
+++ javascript.js
@@ -1,79 +1,120 @@
-const getComputerChoice = () => {
-    let random = Math.floor(Math.random() * 3);
-    switch (random) {
-        case 0:
-            return 'rock';
-        case 1:
-            return 'paper';
-        case 2:
-            return 'scissors';
-    }
+
+function getComputerChoice() {
+  let randomNum = Math.floor(Math.random() * 3);
+
+  switch (randomNum) {
+      case 0:
+          return 'rock';
+      case 1:
+          return 'paper';
+      case 2:
+          return 'scissors';
+  };
 };
 
+const wins = document.querySelector('.winGrid');
+const lose = document.querySelector('.lossGrid');
+const tied = document.querySelector('.tieGrid');
+const games = document.querySelector('.gameProgress');
+const resetBtn = document.querySelector('#reset');
+const btns = document.querySelectorAll('.buttons');
+const startPara = document.querySelector('.firstPara');
+const icon = document.querySelectorAll('i');
 
 let win = 0;
 let loss = 0;
 let tie = 0;
 
-const playRound = (playerSelection, computerSelection) => {
+function playRound(playerChoice, computerChoice) {
 
-    if (playerSelection === computerSelection) {
-        return tie += 1;
-    }
-    if (playerSelection === 'rock') {
-        if (computerSelection === 'paper') {
-            return loss += 1;
-            
-        } else {
-            return win += 1;
-        }
-    }
-    if (playerSelection === 'paper') {
-        if (computerSelection === 'scissors') {
-            return loss += 1;
-        } else {
-            return win += 1;
-        }
-    }
-    if (playerSelection === 'scissors') {
-        if (computerSelection === 'rock') {
-            return loss += 1;
-        } else {
-            return win += 1;
-        }
-    }
+  if (playerChoice === computerChoice) {
+      games.textContent = `Player choice ${playerChoice} ties with Computer choice ${computerChoice}.`
+      return tie += 1;
+  };
+ if (playerChoice === 'rock' && computerChoice === 'scissors'
+ || playerChoice === 'paper' && computerChoice === 'rock'
+ || playerChoice === 'scissors' && computerChoice === 'paper') {
+  games.textContent = `Player choice ${playerChoice} wins Computer choice ${computerChoice}`;
+  return win += 1;
+ } else {
+  games.textContent = `Player choice ${playerChoice} loses to Computer choice ${computerChoice}.`
+  return loss += 1;
+ }
 };
 
-const score = () => {
-    const w = document.querySelector('.win');
-    w.textContent = win;
-    const l = document.querySelector('.loss');
-    l.textContent = loss;
-    const t = document.querySelector('.tie');
-    t.textContent = tie;
-};
-
-function winOrLoss() {
-    if (win === 5) {
-        const declare = document.querySelector('.declaration');
-        declare.textContent = 'You win!'
-        return player.removeEventListener('click', game);
-    } else if (loss === 5) {
-        const declare = document.querySelector('.declaration');
-        declare.textContent = 'Computer wins!';
-        return player.removeEventListener('click', game);
-    }
+function updateScore() {
+  wins.textContent= `Player: ${win}`;
+  lose.textContent = `Computer: ${loss}`;
+  tied.textContent = `Tie: ${tie}`;
 }
 
-const player= document.querySelector('#player');
-
-
-const game = (e) => {
-    const playerSelection = e.target.id;
-    const computerSelection = getComputerChoice();
-    playRound(playerSelection, computerSelection);
-    score();
-    winOrLoss();
+function updateGame() {
+  if (win === 5) {
+      games.textContent = 'You Win! Would you like to start a new game?(y/n)';
+      btns.forEach(btn => btn.removeEventListener('click', game));
+  }
+  if (loss === 5) {
+      games.textContent = 'You lose! Would you like to start a new game?(y/n)'
+      btns.forEach(btn => btn.removeEventListener('click', game));
+  };
 };
 
-player.addEventListener('click', game);
+function resetGame() {
+  win = 0;
+  loss = 0;
+  tie = 0;
+  wins.textContent = 'Win: 0';
+  lose.textContent = 'Loss: 0';
+  tied.textContent = 'Tie: 0';
+  games.textContent = 'First to score 5 points wins the game'
+}
+
+function game(e) {
+  let playerChoice = e.target.id;
+  let computerChoice = getComputerChoice();
+  addIcon(playerChoice);
+  playRound(playerChoice, computerChoice);
+  updateScore();
+  updateGame();
+};
+
+function addIcon(player) {
+  const rock = document.querySelector('.fa-hand-back-fist');
+  const paper = document.querySelector('.fa-hand');
+  const scissor = document.querySelector('.fa-hand-scissors');
+
+  if(player === 'rock' || player === 'Rock') {
+    removeIcon(icon);
+    rock.classList.add('active');
+  } else if(player === 'paper' || player === 'Paper') {
+    removeIcon(icon);
+    paper.classList.add('active')
+  } else {
+    removeIcon(icon);
+    scissor.classList.add('active');
+  }
+}
+
+function removeIcon(icons) {
+  icons.forEach(icon => {
+    if(icon.classList.contains('active')) {
+      icon.classList.remove('active')
+    }
+  })
+}
+
+
+function keydownEvent(e) {
+  let keyboard = e.key;
+  if (keyboard === 'y' || keyboard === 'Y') {
+      resetGame();
+      btns.forEach(btn => btn.addEventListener('click', game));
+  };
+  if (keyboard === 'n' || keyboard === 'N') {
+      btns.forEach(btn => btn.removeEventListener('click', game))
+  }
+};
+
+btns.forEach(btn => btn.addEventListener('click', game));
+resetBtn.addEventListener('click', resetGame);
+document.addEventListener('keydown', keydownEvent);
